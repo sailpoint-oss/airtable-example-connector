3209cfd9d869bfd0a60dad891308bbb739d23966
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-07-14 16:36:22 -0400

    cleaned up mock files

--- src/__mocks__/airtable.ts
+++ src/__mocks__/airtable.ts
@@ -1,8 +1,10 @@
 import { AttributeChange, CompoundKeyType, ConnectorError, ConnectorErrorType, SimpleKeyType, StdAccountCreateInput, StdAccountDiscoverSchemaOutput } from "@sailpoint/connector-sdk"
 import { AirtableAccount } from "../models/AirtableAccount"
 import { AirtableEntitlement } from "../models/AirtableEntitlement"
-import crypto from "crypto"
 import { InvalidConfigurationError } from "../errors/invalid-configuration-error"
+import accountJson from "./account.json"
+import entitlementJson from "./entitlement.json"
+import schemaJson from "./schema.json"
 
 export class AirtableClient {
     constructor(config: any) {
@@ -17,20 +19,10 @@ export class AirtableClient {
     }
 
     async getAllAccounts(): Promise<AirtableAccount[]> { 
-            const recordArray: Array<AirtableAccount> = []
-            const account = new AirtableAccount()
-            account.displayName = "Test User"
-            account.email = "test@test.com"
-            account.id = "1234"
-            account.enabled = true
-            account.locked = false
-            account.department = "accounting"
-            account.firstName = "test"
-            account.lastName = "user"
-            account.password = "password1234"
-            account.entitlments = ["ent1", "ent2"] 
-            recordArray.push(account)
-            return recordArray
+        const recordArray: Array<AirtableAccount> = []
+        const account = Object.assign(new AirtableAccount(), accountJson)
+        recordArray.push(account)
+        return recordArray
     }
 
     async changeAccount(account: AirtableAccount, changes: AttributeChange): Promise<AirtableAccount> {
@@ -42,9 +34,7 @@ export class AirtableClient {
     async getAllEntitlements(): Promise<AirtableEntitlement[]> {
 
             const recordArray: Array<AirtableEntitlement> = []
-            const entitlement = new AirtableEntitlement()
-            entitlement.id = "4321"
-            entitlement.name = "superuser"
+            const entitlement = Object.assign(new AirtableEntitlement(), entitlementJson)
             recordArray.push(entitlement)
             return recordArray
 
@@ -53,18 +43,7 @@ export class AirtableClient {
     async getAccount(identity: SimpleKeyType | CompoundKeyType): Promise<AirtableAccount> {
         const id = <SimpleKeyType>identity
 
-        const account = new AirtableAccount()
-        account.displayName = "Test User"
-        account.email = "test@test.com"
-        account.id = "1234"
-        account.enabled = true
-        account.locked = false
-        account.department = "accounting"
-        account.firstName = "test"
-        account.lastName = "user"
-        account.password = "password1234"
-        account.entitlments = ["ent1", "ent2"] 
-
+        const account = Object.assign(new AirtableAccount(), accountJson)
         if (id.simple.id === "1234") {
             return account
         } else {
@@ -74,37 +53,7 @@ export class AirtableClient {
     }
 
     async getAccountSchema(): Promise<StdAccountDiscoverSchemaOutput> {
-            const recordArray: StdAccountDiscoverSchemaOutput = {
-                "identityAttribute": 'email',
-                "displayAttribute": 'id',
-                "groupAttribute": 'entitlments',
-                "attributes": []
-            }
-            recordArray.attributes = []
-                const fieldset = new AirtableAccount
-                for (const [key] of Object.entries(fieldset)) {
-                    if (key === 'entitlements') {
-                        recordArray.attributes.push(
-                            {
-                                "name": key,
-                                "description": key,
-                                "type": "string",
-                                "entitlement": true,
-                                "managed": true,
-                                "multi": true
-                            }
-                        )
-                    } else {
-                        recordArray.attributes.push(
-                            {
-                                "name": key,
-                                "description": key,
-                                "type": "string"
-                            }
-                        )
-                    }
-                }
-            return recordArray
+            return schemaJson
     }
 
     async deleteAccount(airTableid: string): Promise<Record<string, never>> {
@@ -112,35 +61,14 @@ export class AirtableClient {
     }
 
     async createAccount(input: StdAccountCreateInput): Promise<AirtableAccount> {
-
-        const account = new AirtableAccount()
-        account.displayName = "Test User"
-        account.email = "test@test.com"
-        account.id = "1234"
-        account.enabled = true
-        account.locked = false
-        account.department = "accounting"
-        account.firstName = "test"
-        account.lastName = "user"
-        account.password = "password1234"
-        account.entitlments = ["ent1", "ent2"] 
-
-        return account
-
-
+        return Object.assign(new AirtableAccount(), accountJson)
     }
 
     async getEntitlement(identity: SimpleKeyType | CompoundKeyType): Promise<AirtableEntitlement> {
-
-            const entitlement = new AirtableEntitlement()
-            entitlement.id = "4321"
-            entitlement.name = "superuser"
-
-            return entitlement
+            return Object.assign(new AirtableEntitlement(), entitlementJson)
     }
 
     async testConnection(): Promise<any> {
         return {}
-
     }
 }
